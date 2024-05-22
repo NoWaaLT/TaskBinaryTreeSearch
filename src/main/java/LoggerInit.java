97da81669c5725a9f79dc61bb42c9da94905221d
NoWaaLT <vytautas.jelisejevas@gmail.com>
2024-05-22 17:58:46 +0300

    - Added more unit tests;

---
+++ src/main/java/LoggerInit.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class LoggerInit {
+
+    public final Logger logger = Logger.getLogger("MyLog");
+
+    public LoggerInit(String logFilePath) {
+
+        FileHandler fileHandler;
+
+        try {
+            fileHandler = new FileHandler(logFilePath);
+            logger.addHandler(fileHandler);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fileHandler.setFormatter(formatter);
+            logger.info("Logger Initialized");
+
+        } catch (IOException e) {
+            logger.log(Level.WARNING, "Exception ::", e);
+        }
+    }
+}
